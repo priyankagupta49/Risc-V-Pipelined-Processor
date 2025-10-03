158d8642c17110af555c3b03d3786e49687253c0
priyankagupta49 <163023910+priyankagupta49@users.noreply.github.com>
2025-10-03 22:48:24 +0530

    Update PC.v

--- PC.v
+++ PC.v
@@ -2,14 +2,15 @@ module PC_Module(
     input clk,
     input rst,
     input [31:0] PC_Next,
-    output reg [31:0] PC  // Declaring PC as reg because it is updated inside always block
+    output reg [31:0] PC  
 );
 
     always @(posedge clk) begin
         if (rst == 1'b0)
-            PC <= 32'b0;  // Initialize PC to 0 on reset
+            PC <= 32'b0;  
         else
             PC <= PC_Next;
     end
 
 endmodule
+
