b07e4aa6c063e4cfdd0a64b9456b264d4ee73961
priyankagupta49 <163023910+priyankagupta49@users.noreply.github.com>
2025-10-03 23:02:10 +0530

    Update Writeback_Cycle.v

--- Writeback_Cycle.v
+++ Writeback_Cycle.v
@@ -1,14 +1,13 @@
 module writeback_cycle(
     input clk,
     input rst,
-    input ResultSrcW,          // Ensure this is a 1-bit signal
+    input ResultSrcW,          
     input [31:0] PCPlus4W,
     input [31:0] ALU_ResultW,
     input [31:0] ReadDataW,
     output [31:0] ResultW
 );
 
-// Mux instantiation for selecting between ALU result and Read Data
 Mux result_mux (    
     .a(ALU_ResultW),
     .b(ReadDataW),
