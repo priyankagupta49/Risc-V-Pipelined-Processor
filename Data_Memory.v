402f7e79d529354d3c388c14e26614120ea6c9d3
priyankagupta49 <163023910+priyankagupta49@users.noreply.github.com>
2025-10-04 00:26:16 +0530

    Update Data_Memory.v

--- Data_Memory.v
+++ Data_Memory.v
@@ -4,16 +4,13 @@ module Data_Memory(
     output reg [31:0] RD
 );
 
-    // Declare memory outside of any procedural block
     reg [31:0] mem [1023:0];
 
-    // Memory write operation on clock edge
     always @(posedge clk) begin
         if (WE)
             mem[A] <= WD;
     end
-
-    //  Read operation 
+ 
     always @(*) begin
         if (~rst)
             RD = 32'd0;
@@ -21,11 +18,11 @@ module Data_Memory(
             RD = mem[A];
     end
 
-    // Initialize memory
     initial begin
        // mem[0] = 32'h00000002;
         mem[1] = 32'h00000006;
     end
 
 endmodule
+
 
