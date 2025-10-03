0f693ff896f5bdbce74fdad4009cd5739de93af0
priyankagupta49 <163023910+priyankagupta49@users.noreply.github.com>
2025-10-04 00:36:14 +0530

    Update Hazard_unit.v

--- Hazard_unit.v
+++ Hazard_unit.v
@@ -1,7 +1,6 @@
 
 module hazard_unit(rst, RegWriteM, RegWriteW, RD_M, RD_W, Rs1_E, Rs2_E, ForwardAE, ForwardBE);
 
-    // Declaration of I/Os
     input rst, RegWriteM, RegWriteW;
     input [4:0] RD_M, RD_W, Rs1_E, Rs2_E;
     output [1:0] ForwardAE, ForwardBE;
@@ -13,5 +12,6 @@ module hazard_unit(rst, RegWriteM, RegWriteW, RD_M, RD_W, Rs1_E, Rs2_E, ForwardA
     assign ForwardBE = (rst == 1'b0) ? 2'b00 : 
                        ((RegWriteM == 1'b1) & (RD_M != 5'h00) & (RD_M == Rs2_E)) ? 2'b10 :
                        ((RegWriteW == 1'b1) & (RD_W != 5'h00) & (RD_W == Rs2_E)) ? 2'b01 : 2'b00;
+
 
 endmodule
