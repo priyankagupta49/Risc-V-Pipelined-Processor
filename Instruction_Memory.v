c12b9507534e1b61156540d29a2cb48ce05e5025
priyankagupta49 <163023910+priyankagupta49@users.noreply.github.com>
2025-10-06 18:11:18 +0530

    Update Instruction_Memory.v

--- Instruction_Memory.v
+++ Instruction_Memory.v
@@ -10,14 +10,15 @@ module Instruction_Memory(
     assign RD = (rst == 1'b0) ? 32'b0 : mem[A[31:2]];
 
     initial begin
-        mem[0] = 32'hFFC4A303; 
-        mem[1] = 32'h00832383; 
-        mem[2] = 32'h0064A423;
-        mem[3] = 32'h00B62423;
-        mem[4] = 32'h0062E233;
-        mem[5] = 32'h00B62423;
+        mem[0] = 32'h00500093; // addi x1, x0, 5
+        mem[1] = 32'h00A00113; // addi x2, x0, 10
+        mem[2] = 32'h002081B3; // add x3, x1, x2
+        mem[3] = 32'h06400213; // addi x4, x0, 100
+        mem[4] = 32'h00322023; // sw x3, 0(x4)
+        mem[5] = 32'h00022283; // lw x5, 0(x4)
     end
 
 endmodule
+
 
 
