8a49e28d36e1f65f7367f87ccbd459d79dcdd0ac
priyankagupta49 <163023910+priyankagupta49@users.noreply.github.com>
2025-10-06 16:52:05 +0530

    Update Hazard_unit.v

--- Hazard_unit.v
+++ Hazard_unit.v
@@ -6,12 +6,13 @@ module hazard_unit(rst, RegWriteM, RegWriteW, RD_M, RD_W, Rs1_E, Rs2_E, ForwardA
     output [1:0] ForwardAE, ForwardBE;
     
     assign ForwardAE = (rst == 1'b0) ? 2'b00 : 
-                       ((RegWriteM == 1'b1) & (RD_M != 5'h00) & (RD_M == Rs1_E)) ? 2'b10 :
-                       ((RegWriteW == 1'b1) & (RD_W != 5'h00) & (RD_W == Rs1_E)) ? 2'b01 : 2'b00;
+        ((RegWriteM == 1'b1) && (RD_M != 5'h00) && (RD_M == Rs1_E)) ? 2'b10 :
+        ((RegWriteW == 1'b1) && (RD_W != 5'h00) && (RD_W == Rs1_E)) ? 2'b01 : 2'b00;
                        
     assign ForwardBE = (rst == 1'b0) ? 2'b00 : 
-                       ((RegWriteM == 1'b1) & (RD_M != 5'h00) & (RD_M == Rs2_E)) ? 2'b10 :
-                       ((RegWriteW == 1'b1) & (RD_W != 5'h00) & (RD_W == Rs2_E)) ? 2'b01 : 2'b00;
+        ((RegWriteM == 1'b1) && (RD_M != 5'h00) && (RD_M == Rs2_E)) ? 2'b10 :
+        ((RegWriteW == 1'b1) && (RD_W != 5'h00) && (RD_W == Rs2_E)) ? 2'b01 : 2'b00;
 
 
 endmodule
+
